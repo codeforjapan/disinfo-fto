524b5c81cd16d8bd0df99dd1922ae56d25e0b34d
KazuumiN <nishimura@liquitous.com>
2024-08-17 16:10:04 +0900

    update: adjust placeholder

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 						type="text"
 						required
 						class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-						placeholder="https://example.com"
+						placeholder="https://twitter.com/<some-handler>/status/<id-number>"
 					/>
 				</div>
 			</div>
