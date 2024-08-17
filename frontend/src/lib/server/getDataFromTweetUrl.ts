2d36cc38b65be5dd2da1c72a242c2d121b8d9ac5
MrOrz <johnsonliang7@gmail.com>
2024-08-17 17:47:01 +0900

    fix(lib): fix issues in getDataFromTweetUrls: 1. Should only extract t.co URLs 2. Prettier fix

--- frontend/src/lib/server/getDataFromTweetUrl.ts
+++ frontend/src/lib/server/getDataFromTweetUrl.ts
@@ -1,25 +1,26 @@
-
 /**
- * Fetch tweet content from Twitter oEmbed API and extract URLs inside the tweet
+ * Fetch tweet content from Twitter oEmbed API and extract URLs inside the tweet.
+ * External URLs are using t.co as host.
  *
  * @param tweetUrl - The URL of the tweet
  * @returns An array of URLs inside the tweet
  */
 async function extractUrlsFromTweetUrl(tweetUrl: string): Promise<string[]> {
-  const response = await fetch(`https://publish.twitter.com/oembed?url=${tweetUrl}`)
-  const data = await response.json()
-  return data.html.match(/href="(https?:\/\/[^\s]+)"/g) || []
+	const response = await fetch(`https://publish.twitter.com/oembed?url=${tweetUrl}`);
+	const data = await response.json();
+	const allUrls = [...(data.html as string).matchAll(/href="(https?:\/\/[^\s]+)"/g)];
+	return allUrls.map((m) => m[1]).filter((url) => url.startsWith('https://t.co/'));
 }
 
 type Data = Array<{
-  url: string;
-  // TODO: Define the rest of the data structure
+	url: string;
+	// TODO: Define the rest of the data structure
 }>;
 
 export default async function getDataFromTweetUrl(url: string): Promise<Data> {
-  const urls = await extractUrlsFromTweetUrl(url);
+	const urls = await extractUrlsFromTweetUrl(url);
 
-  // TODO: invoke BirdXplorer API to get posts with these URLs
+	// TODO: invoke BirdXplorer API to get posts with these URLs
 
-  return urls.map((url) => ({ url }));
+	return urls.map((url) => ({ url }));
 }
