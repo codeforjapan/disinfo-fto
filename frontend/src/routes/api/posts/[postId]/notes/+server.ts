67e8a8538b00fad43d9046ea94ddf06eb044482a
KazuumiN <nishimura@liquitous.com>
2024-08-18 11:17:21 +0900

    fix: removed hardcoded language parameter

--- frontend/src/routes/api/posts/[postId]/notes/+server.ts
+++ frontend/src/routes/api/posts/[postId]/notes/+server.ts
@@ -5,7 +5,7 @@ const BASE_URL = 'https://birdxplorer.onrender.com';
 
 export const GET = async ({ params }) => {
     const postId = params.postId;
-    const res = await fetch(`${BASE_URL}/api/v1/data/notes?post_ids=${postId}&language=ja`).then(res => res.json())
+    const res = await fetch(`${BASE_URL}/api/v1/data/notes?post_ids=${postId}`).then(res => res.json())
     return json({
         notes: res.data as Note[]
     })
